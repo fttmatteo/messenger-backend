f60d60965f19db255fcd2e879468a4ab11aeaec9
fttmatteo <valenciaardila988@icloud.com>
2025-12-03 19:03:46 -0500

    MD service Employee

--- messenger/src/main/java/app/domain/model/Employee.java
+++ messenger/src/main/java/app/domain/model/Employee.java
@@ -1,5 +1,33 @@
 package app.domain.model;
 
-public class Employee {
+import app.domain.model.enums.Role;
 
+public class Employee {
+    private String userName;
+    private String password;
+    private Role role;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
