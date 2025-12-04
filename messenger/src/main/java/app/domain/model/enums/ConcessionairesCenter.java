e41363fc1409b741c8a23d9bfab5fb743505e9e9
fttmatteo <valenciaardila988@icloud.com>
2025-12-03 19:02:54 -0500

    ADD enums ConcesionarieType, Role, Status and TypePlate

---
+++ messenger/src/main/java/app/domain/model/enums/ConcessionairesCenter.java
@@ -0,0 +1,12 @@
+package app.domain.model.enums;
+
+public enum ConcessionairesCenter {
+    ALK_33,
+    ALK_332,
+    ALK_333,
+    HERO,
+    INCOLMOTOS,
+    DISMERCA,
+    EXITO_BELEN,
+    TOMA_TU_MOTO
+}
---
+++ messenger/src/main/java/app/domain/model/enums/Status.java
@@ -0,0 +1,10 @@
+package app.domain.model.enums;
+
+public enum Status {
+    PENDING,
+    ASSIGNED,
+    IN_PROCESS,
+    DELIVERED,
+    RETURNED,
+    OBSERVED
+}
