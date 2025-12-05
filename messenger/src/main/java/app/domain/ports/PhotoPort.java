41be2fe3acfdf644396acf3117d1aa0fd07556f0
fttmatteo <valenciaardila988@icloud.com>
2025-12-04 19:48:38 -0500

    ADD ports PhotoPort

---
+++ messenger/src/main/java/app/domain/ports/PhotoPort.java
@@ -0,0 +1,8 @@
+package app.domain.ports;
+
+import app.domain.model.Photo;
+
+public interface PhotoPort {
+    void save(Photo photo) throws Exception;
+    Photo findById(Long id) throws Exception;
+}   
