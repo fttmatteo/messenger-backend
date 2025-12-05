21f8949ef5d290027d35e95b64a108d9b015f481
fttmatteo <valenciaardila988@icloud.com>
2025-12-04 19:38:47 -0500

    MD ports EmployeePort

--- messenger/src/main/java/app/domain/ports/EmployeePort.java
+++ messenger/src/main/java/app/domain/ports/EmployeePort.java
@@ -4,7 +4,6 @@ import app.domain.model.Employee;
 
 public interface EmployeePort {
     Employee findByDocument(Employee employee) throws Exception;
-    Employee findByUserName(Employee employee) throws Exception;
     void save(Employee employee) throws Exception;
     void deleteByDocument(Long document) throws Exception;
     void update(Employee employee) throws Exception;
