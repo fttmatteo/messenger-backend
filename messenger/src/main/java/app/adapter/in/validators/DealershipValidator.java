5affc2e40a681840b054c4f47a0d10d7627e3644
fttmatteo <valenciaardila988@icloud.com>
2025-12-06 17:58:08 -0500

    ADD DealershipBuilder

--- messenger/src/main/java/app/adapter/in/validators/DealershipValidator.java
+++ messenger/src/main/java/app/adapter/in/validators/DealershipValidator.java
@@ -1,7 +1,9 @@
 package app.adapter.in.validators;
 
+import org.springframework.stereotype.Component;
 import app.application.exceptions.InputsException;
 
+@Component
 public class DealershipValidator extends SimpleValidator {
     public String nameValidator(String value) throws InputsException {
         return stringValidator("nombre", value);
