d5d413607f207e5309fd7a76aafbb980a93a3beb
fttmatteo <valenciaardila988@icloud.com>
2025-12-08 19:42:02 -0500

    DealershipValidator

---
+++ messenger/src/main/java/app/adapter/in/validators/DealershipValidator.java
@@ -0,0 +1,32 @@
+package app.adapter.in.validators;
+
+import org.springframework.stereotype.Component;
+import app.application.exceptions.InputsException;
+
+@Component
+public class DealershipValidator extends SimpleValidator {
+
+    public String nameValidator(String value) throws InputsException {
+        return stringValidator("nombre del concesionario", value);
+    }
+
+    public String addressValidator(String value) throws InputsException {
+        return stringValidator("dirección", value);
+    }
+
+    public String phoneValidator(String value) throws InputsException {
+        stringValidator("teléfono", value);
+        if (!value.matches("\\d{10}")) {
+            throw new InputsException("El teléfono del concesionario debe tener 10 dígitos.");
+        }
+        return value;
+    }
+
+    public String zoneValidator(String value) throws InputsException {
+        stringValidator("zona", value);
+        if (!value.equalsIgnoreCase("centro") && !value.equalsIgnoreCase("sur") && !value.equalsIgnoreCase("norte")) {
+            throw new InputsException("La zona debe ser centro, sur o norte");
+        }
+        return value;
+    }
+}
