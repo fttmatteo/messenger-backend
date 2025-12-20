a6cc3e38dda3e8fda92b78cdef34eeea4cde46fd
fttmatteo <valenciaardila988@icloud.com>
2025-12-20 15:19:12 -0500

    config: Exclude UserDetailsServiceAutoConfiguration from Spring Boot application.

--- messenger/src/main/java/app/MessengerApplication.java
+++ messenger/src/main/java/app/MessengerApplication.java
@@ -3,19 +3,8 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-<<<<<<< develop
-@SpringBootApplication
-=======
-/**
- * Clase principal de la aplicación Messenger Backend.
- * Esta es la clase de entrada que inicia la aplicación Spring Boot.
- * Gestiona el sistema de entregas de placas vehiculares, permitiendo
- * el registro, tracking y gestión de servicios de mensajería.
- */
-
 @SpringBootApplication(excludeName = {
 		"org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration" })
->>>>>>> main
 public class MessengerApplication {
 
 	public static void main(String[] args) {
