7b024c1fd7d1899ab082482c370f6e676d9cf29a
fttmatteo <valenciaardila988@icloud.com>
2025-12-23 13:34:37 -0500

    refactor: remove unused `@Component` import in `AdminSeeder`.

--- messenger/src/main/java/app/infrastructure/persistence/seeder/AdminSeeder.java
+++ messenger/src/main/java/app/infrastructure/persistence/seeder/AdminSeeder.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 import app.domain.model.enums.Role;
 import app.infrastructure.persistence.entities.EmployeeEntity;
 import app.infrastructure.persistence.repository.EmployeeRepository;
