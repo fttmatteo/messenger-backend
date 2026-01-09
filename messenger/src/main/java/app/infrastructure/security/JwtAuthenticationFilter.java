1a6d4311e3240aea30f875a3fa0b151cbf1167d4
fttmatteo <valenciaardila988@icloud.com>
2026-01-09 15:34:09 -0500

    fix(auth): fix compilation error and simplify token extraction in JwtAuthenticationFilter

--- messenger/src/main/java/app/infrastructure/security/JwtAuthenticationFilter.java
+++ messenger/src/main/java/app/infrastructure/security/JwtAuthenticationFilter.java
@@ -2,7 +2,6 @@ package app.infrastructure.security;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
